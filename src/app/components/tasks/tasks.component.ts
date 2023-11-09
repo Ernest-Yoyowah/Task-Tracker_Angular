2d51cb1ee360efc0e455f9c088233f18a220d43e
ernkeyz <exconzy26@gmail.com>
2023-11-09 11:52:12 +0000

    Front-End Complete

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from '../../Task';
-import { TASKS } from '../../mock_tasks';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, observable, of } from 'rxjs';
+import { Task } from '../Task';
+import { TASKS } from '../mock_tasks';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  constructor() {}
+
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
